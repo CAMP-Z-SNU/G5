bdc53695279eada30c8b0831934b50b190176c49
KusumithaBannur <bannurkusumitha@gmail.com>
2024-09-04 20:08:51 +0530

    Add files via upload

---
+++ 3/fibonacci1.java
@@ -0,0 +1,23 @@
+//using recursion method
+import java.util.Scanner;
+public class fibonacci1{
+
+    public static int fib(int n){
+
+        if(n<=1){
+            return n;
+        }
+        return fib(n-1)+fib(n-2);
+    }
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter value of n : ");
+        int n = sc.nextInt();
+
+        System.out.println("The number "+n+" of fibonacci series is : "+fib(n));
+        sc.close();
+    }
+
+}
